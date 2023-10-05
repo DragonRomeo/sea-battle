ecc56356e405913e493e97be600275b91fa025b8
Ilya Dragonromeo <you@example.com>
2023-10-05 16:47:53 +0300

    refactor: remove exta components from core

--- sea battle/src/core/components/Paragraph/Paragraph.tsx
+++
@@ -1,9 +0,0 @@
-import type { FC } from 'react';
-
-interface Props {
-  text: string;
-}
-
-export const Paragraph: FC<Props> = ({ text }) => {
-  return <p>{text}</p>;
-};
--- sea battle/src/pages/StartPage/TestPage.tsx
+++
@@ -1,7 +0,0 @@
-import type { FC } from 'react';
-import styles from './StartPage.module.css'
-
-
-export const TestPage: FC = () => {
-  return <div className={styles.div}>ханты-мантыйс</div>;
-};
