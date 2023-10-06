f2fb2949bb0568de3f156a0976fba70837aee5b5
Ilya Dragonromeo <you@example.com>
2023-10-06 16:14:19 +0300

    options: add components names for classNames

--- sea battle/vite.config.ts
+++ sea battle/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const isProd = mode === 'production';
+
+  return {
+    plugins: [react()],
+    css: {
+      modules: {
+        // If environment is not Prod include the name of the component for stylesheets.
+        generateScopedName: isProd
+          ? `[hash:base64:8]`
+          : `[name]_[local]_[hash:base64:5]`,
+      },
+    },
+  };
+});
