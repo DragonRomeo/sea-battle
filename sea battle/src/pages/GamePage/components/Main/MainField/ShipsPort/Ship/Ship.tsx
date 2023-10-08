74d68ef666c94293801c2d30aa315dca017a3c27
Ilya Dragonromeo <you@example.com>
2023-10-08 16:42:18 +0300

    feat: add rotate for ships

--- sea battle/src/pages/GamePage/components/Main/MainField/ShipsPort/Ship/Ship.tsx
+++ sea battle/src/pages/GamePage/components/Main/MainField/ShipsPort/Ship/Ship.tsx
@@ -1,14 +1,27 @@
 import style from './Ship.module.css';
-import { FC } from 'react';
+import type { FC } from 'react';
+import { useState } from 'react';
 
 interface Props {
   className: string;
 }
 export const Ship: FC<Props> = ({ className }) => {
+  const [rotate, setRotate] = useState(false);
+
+  const styleRotate = {
+    transform: rotate ? 'rotate(90deg)' : '',
+    transition: 'transform 150ms ease',
+  };
+
   return (
     <>
-      <div className={`${style.ship} ${className}`}>Ship</div>
-      
+      <div
+        className={`${style.ship} ${className}`}
+        style={styleRotate}
+        onClick={() => setRotate(!rotate)}
+      >
+        Ship
+      </div>
     </>
   );
 };
