ee6dd3d29252ff522537d70d1a6a273bc80ce8ed
Ilya Dragonromeo <you@example.com>
2023-10-08 16:12:42 +0300

    feat: add ships

--- sea battle/src/pages/GamePage/components/Main/MainField/ShipsPort/Ship/Ship.tsx
+++ sea battle/src/pages/GamePage/components/Main/MainField/ShipsPort/Ship/Ship.tsx
@@ -1,6 +1,14 @@
+import style from './Ship.module.css';
+import { FC } from 'react';
 
-export const Ship = () => {
-  return (
-    <div>Ship</div>
-  )
+interface Props {
+  className: string;
 }
+export const Ship: FC<Props> = ({ className }) => {
+  return (
+    <>
+      <div className={`${style.ship} ${className}`}>Ship</div>
+      
+    </>
+  );
+};
--- sea battle/src/pages/GamePage/components/Main/MainField/ShipsPort/ShipsPort.tsx
+++ sea battle/src/pages/GamePage/components/Main/MainField/ShipsPort/ShipsPort.tsx
@@ -1,5 +1,24 @@
+import { Ship } from './Ship/Ship';
 import style from './ShipsPort.module.css';
 
+const ships = {
+  shipVeryBig: 1,
+  shipLarge: 2,
+  shipMedium: 3,
+  shipSmall: 4,
+};
+
 export const ShipsPort = () => {
-  return <div className={style.container}></div>;
+  const shipsRender: Array<JSX.Element> = [];
+
+  Object.entries(ships).forEach((item) => {
+    for (let i = 0; i < item[1]; i++) {
+      console.log(i);
+      shipsRender.push(
+        <Ship key={`${item[0]}${item[1]}${i}`} className={style[item[0]]} />
+      );
+    }
+  });
+
+  return <div className={style.container}>{shipsRender}</div>;
 };
